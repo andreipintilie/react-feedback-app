0d81490e41260ec3f11a93aa41450fc65b05915c
Pintilie2000 <andrei.pintilie00@e-uvt.ro>
2021-12-23 18:08:59 +0200

    First Commit

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,24 @@
+const FeedbackData = [
+	{
+		id: 1,
+		rating: 10,
+		text: 'This is a great app',
+	},
+	{
+		id: 2,
+		rating: 9,
+		text: 'This is a good app',
+	},
+	{
+		id: 3,
+		rating: 8,
+		text: 'This is a ok app',
+	},
+	{
+		id: 4,
+		rating: 7,
+		text: 'This is a bad app',
+	},
+];
+
+export default FeedbackData;
